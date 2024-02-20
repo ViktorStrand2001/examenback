d6d0d02ab78f600c6b531ec1bb6620c6be1aaa64
Viktor Strand <102230210+ViktorStrand2001@users.noreply.github.com>
2024-02-20 14:48:32 +0100

    Initial commit

---
+++ src/main/java/com/example/examenback/ExamenbackApplication.java
@@ -0,0 +1,13 @@
+package com.example.examenback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenbackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamenbackApplication.class, args);
+	}
+
+}
